b33f6b3f85cb73a7ff76b3095c0e66ecb1dd0808
Stella Lu <stellalu@berkeley.edu>
2019-04-14 00:01:52 -0700

    Got notes from meeting with Karl

--- main.py
+++ main.py
@@ -4,19 +4,35 @@ lambdaVal = 69
 n = 3
 x = [1, 2, 3] # Should be of length n
 
+rand = np.random.randn(n, n)
+# A is positive definite 
+A = rand @ rand.T + np.eye(n) * lambdaVal
 
-B = np.random.randn(n, n)
-A = (B @ np.matrix.transpose(B)) + np.eye(n) + lambdaVal
-# A is positive definite n by n matrix
+def f(x):
+	# f(x) = x^T*A*x
+	return 0.5 * x.T @ A @ x
 
-A_mul_x = np.dot(A, x)
-f = np.dot(np.matrix.transpose(np.array(x)), A_mul_x)
+# Initial x and B values
+x = np.random.randn(n) * 1000.0
+B = np.eye(n)
 
-print(A)
-print("matrix A_mul_x: ")
-print(A_mul_x)
-print("x:")
-print(np.array(x))
-print("f(x) = ", f)
+def bfgs():
+	# derivative of f = 2Ax
+	f_derivative = np.dot(A, x)
+	p_k = -B*f_derivative
 
-print(type(f))
+
+
+	# Line search in direction p_k to find x_k+1 (golden section search, fixed step size)
+
+
+	# Find s_k, y_k, p_k, 
+
+
+
+
+
+# Test that BFGS works
+# 1. Collect all xs throughout the descent in a list
+# 2. plot f(x) over time gets smaller
+# 3. Make sure f(x) goes to 0
